35d6a739e10f60ffd9edc7383fc37a8a6fba3e4c
Thor181 <art.torgaschin@yandex.ru>
2023-10-08 00:31:31 +0700

    Добавьте файлы проекта.

---
+++ LogicitApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace LogicitApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ LogicitApp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,32 @@
+﻿using LogicitApp.Stuff.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace LogicitApp.ViewModels
+{
+    public class LoginViewModel
+    {
+        public SimpleCommand LoginCommand { get; set; }
+
+        public string LoginString { get; set; }
+        public string PasswordString { get; set; }
+
+        public LoginViewModel()
+        {
+            LoginCommand = new SimpleCommand(LoginHandler);
+        }
+
+        private void LoginHandler(object? parameter)
+        {
+            var sec = parameter as PasswordBox;
+            
+          
+            var JUST_BREAKPOINT = string.Empty; 
+        }
+    }
+}
